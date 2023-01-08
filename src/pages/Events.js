596dee01625a3c9c06c7792c44bede36afe38a7c
Sarvesh Shirude <102407237+esarvesh11@users.noreply.github.com>
2023-01-08 09:28:12 +0530

    docs

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -6,7 +6,7 @@ function Events() {
       <div class="">
 
         <div class="gap-8 max-w-2xl mx-auto py-4 px-4 sm:py-2 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 class="text-3xl font-extrabold uppercase  text-gray-200 tracking-wide mb-4">MAIN FEST <span class='text-blue-400'>EVENTS</span> 2k23-24</h2>
+          <h2 class="text-3xl font-extrabold uppercase  text-gray-200 tracking-wide mb-4">MAIN FEST <span class='text-blue-400'>EVENTS</span> 2k22-23</h2>
         </div>
         {/* displaying cards  */}
         <div class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center px-24 transition-all l">
